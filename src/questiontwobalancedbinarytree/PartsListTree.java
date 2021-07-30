361088c166458144ac948ce2c3c6f1615e981faa
Caspian Maclean <caspianmaclean@gmail.com>
2021-07-30 09:45:30 +0800

    Added placeholder methods for the binary tree

---
+++ src/questiontwobalancedbinarytree/PartsListTree.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questiontwobalancedbinarytree;
+
+/**
+ *
+ * @author Caspian Maclean 30039802
+ * 
+ * Class to hold a list of mechanical parts in a balanced binary tree.
+ * 
+ */
+public class PartsListTree {
+    public void add(String part) {
+        
+    }
+    
+    public void remove(String part) {
+        
+    }
+    
+    public boolean contains(String part) {
+        return false; // Placeholder value for unimplemented method
+    }
+}
