e33a72c3c3ecd61b583677e675edd168db1c0b74
Caspian Maclean <caspianmaclean@gmail.com>
2021-07-30 09:39:23 +0800

    Fixed up comments

--- src/questiontwobalancedbinarytree/QuestionTwoBalancedBinaryTree.java
+++ src/questiontwobalancedbinarytree/QuestionTwoBalancedBinaryTree.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package questiontwobalancedbinarytree;
 
 /**
  *
- * @author caspian
+ * @author Caspian Maclean 30039802
+ * 30/7/2021
+ * 
+ * Balanced Binary Tree example
+ * 
+ * Question 2 – You are required to make a mechanical parts list sorted
+ * alphabetically.
+ * You need to create a balanced binary search tree for a dictionary of no less
+ * than 10 words. You must be able to search the list, add and remove from the
+ * list.
  */
 public class QuestionTwoBalancedBinaryTree {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
