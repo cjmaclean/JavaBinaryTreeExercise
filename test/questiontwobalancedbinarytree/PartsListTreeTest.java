1b471b922d45904ce74530de9626b6ed90f3c888
Caspian Maclean <caspianmaclean@gmail.com>
2021-07-30 09:59:05 +0800

    Add some tests

---
+++ test/questiontwobalancedbinarytree/PartsListTreeTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questiontwobalancedbinarytree;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author caspian
+ */
+public class PartsListTreeTest {
+
+    /**
+     * Test an empty parts list tree.
+     */
+    @Test
+    public void testEmptyTree() {
+        PartsListTree tree = new PartsListTree();
+        assertFalse(tree.contains("gearbox"));
+    }
+
+    /**
+     * Test of add method, of class PartsListTree.
+     */
+    @Test
+    public void testAdd() {
+        PartsListTree tree = new PartsListTree();
+        tree.add("piston");
+        tree.add("axle");
+        assertTrue(tree.contains("piston"));
+        assertTrue(tree.contains("axle"));
+    }
+
+    /**
+     * Test of remove method, of class PartsListTree.
+     */
+    @Test
+    public void testRemove() {
+        PartsListTree tree = new PartsListTree();
+        tree.add("piston");
+        tree.add("axle");
+        tree.remove("piston");
+        assertFalse(tree.contains("piston"));
+        assertTrue(tree.contains("axle"));
+    }
+
+    /**
+     * Test of contains method, of class PartsListTree.
+     */
+    @Test
+    public void testContains() {
+        PartsListTree tree = new PartsListTree();
+        tree.add("piston");
+        tree.add("axle");
+        assertTrue(tree.contains("piston"));
+        assertTrue(tree.contains("axle"));
+        assertFalse(tree.contains("gearbox"));
+        assertFalse(tree.contains("acksell"));
+    }
+
+}
